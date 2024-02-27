b92f4daae14095a47717f67cfa8098e212e78449
Vorshim92 <vorshim92@gmail.com>
2024-02-27 19:51:32 +0100

    added sessionStorage for appointments

--- js/index.js
+++ js/index.js
@@ -4,76 +4,86 @@
 
 //  ✓ 2) Con il numero esatto, trovato in precedenza, di giorni in questo mese dobbiamo generare il numero corrispondente di celle nel calendario
 
-//  3) Il giorno corrente dovrebbe "illuminarsi"
+//  ✓ 3) Il giorno corrente dovrebbe "illuminarsi"
 
-//  4) Visualizzare il nome del mese nell'h1
+//  ✓ 4) Visualizzare il nome del mese nell'h1
 
 //  5) Rendere cliccabili le celle per poter salvare gli appuntamenti in quel dato giorno (applicare un bordo alla selezione)
 
 // ________________________________
 
-//  6) Modificare il meeting day alla selezione di un giorno
+//  ✓ 6) Modificare il meeting day alla selezione di un giorno
 
 //  7) Salvare un appuntamento con l'ora e una stringa sempre per il giorno selezionato
 
 //  8) devo poter selezionare altri giorni, potendo inserire un appuntamento per loro, pur mantenendo la possibilità di RIVEDERE appuntamenti già creati su altri giorni
 
 //  extra 9) un giorno contenente un appuntamento dovrà riflettere questo stato con un pallino colorato dentro la cella
+// creo una storagekey per il sessionStorage
+const storageKey = `appointments`;
 
-// _____________________________________________________________________________________________________________________________
+//  creo una classe costruttrice
+class MeetingEvent {
+  constructor(_day, _month, _time, _name) {
+    this.day = _day;
+    this.month = _month;
+    this.time = _time;
+    this.name = _name;
+  }
+}
+function createEvent() {
+  let time = document.getElementById("newMeetingTime").value;
+  let name = document.getElementById("newMeetingName").value;
+  let month = monthNames[actualMonth];
+  const newMeetingEvent = new MeetingEvent(lastSelDay, month, time, name);
+  const savedAppointments = sessionStorage.getItem(storageKey);
 
+  if (savedAppointments) {
+    const tempArray = JSON.parse(savedAppointments);
+    tempArray.push(newMeetingEvent);
+    sessionStorage.setItem(storageKey, JSON.stringify(tempArray));
+  } else {
+    const appointments = [];
+    appointments.push(newMeetingEvent);
+    sessionStorage.setItem(storageKey, JSON.stringify(appointments));
+  }
+}
 // 7a)
-const appointments = [];
 
-/* vedi ciclo for per la generazione delle celle interne dell'array
-[
-    [], [], [], [], [], [], [], 
-    [], [], [], [], [], [], [], 
-    [], [], [], [], [], [], [], 
-    [], [], [], [], [], [], [], 
-    [], 
-] 
-*/
 const now = new Date(); // estrae il momento nel tempo attuale in cui si esegue in forma di data
 let actualMonth = now.getMonth();
 let actualYear = now.getFullYear();
 let lastSelMonthYear;
 let lastSelDay;
-const monthNames = [
-  "Gennaio",
-  "Febbraio",
-  "Marzo",
-  "Aprile",
-  "Maggio",
-  "Giugno",
-  "Luglio",
-  "Agosto",
-  "Settembre",
-  "Ottobre",
-  "Novembre",
-  "Dicembre",
-];
-const dayNames = [
-  "Domenica",
-  "Lunedì",
-  "Martedì",
-  "Mercoledì",
-  "Giovedì",
-  "Venerdì",
-  "Sabato",
-];
-// 1) abbiamo il valore numerico, di nr di giorni in questo mese, in uscita dalla funzione daysInThisMonth
+const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+const dayNames = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
 
-// 5b) deselezionare eventuali altri elementi selezionati
+const addMettingEvent = function (e) {
+  e.preventDefault();
+  createEvent();
+  form.reset();
+};
 
-// 6) cambiare numero in newMeetingDay
+const form = document.getElementById("meeting");
+form.addEventListener("submit", addMettingEvent);
 
-// 8) creo una funzione per gestire la visualizzazione dei miei appuntamenti
-const showAppointments = (dayIndex) => {};
+const showAppointments = (selDay, selMonth) => {
+  const savedEvents = sessionStorage.getItem(storageKey);
+  const uList = document.getElementById("appointmentsList");
 
-// 7c) salvare appuntamento
-const saveMeeting = function (e) {
-  // da fare sempre per una funzione collegata al submit di un form
+  if (storageKey) {
+    const savedEventsArr = JSON.parse(savedEvents);
+    const dayAppointments = savedEventsArr.filter((obj) => parseInt(obj.day) === selDay && obj.month === selMonth);
+    console.log(dayAppointments);
+    dayAppointments.forEach((appointment) => {
+      const newLi = document.createElement("li");
+      document.querySelector(".appointments").classList.remove("appointments");
+      newLi.innerText = `ORA: ${appointment.time} - NOME: ${appointment.name}`;
+      uList.appendChild(newLi);
+    });
+  } else {
+    uList.innerHTML = ``;
+  }
 };
 
 // 2) creazione dinamica delle celle in base al mese e anno in arrivo come parametro
@@ -115,6 +125,7 @@ const createDaysAndMonth = (month, year) => {
   restoreSelection(h1.innerHTML);
 };
 
+// funzione per aggiungere classe ACTUAL al giorno corrente, solo quando ci troviamo nel mese corrente
 const currentDay = (month, year) => {
   if (month === now.getMonth() && year === now.getFullYear()) {
     let allDay = document.querySelectorAll("#giorno");
@@ -126,6 +137,7 @@ const currentDay = (month, year) => {
   }
 };
 
+// CONDIZIONE PER RIPRISTINARE LA SELEZIONE NEL MESE IN CUI E' STATA SELEZIONATA
 const restoreSelection = (monthYear) => {
   if (monthYear === lastSelMonthYear) {
     let allDays = document.querySelectorAll(".day");
@@ -137,31 +149,36 @@ const restoreSelection = (monthYear) => {
   }
 };
 
+// INIZIALIZZAZIONE EVENTLISTENER su OGNI DIV per il CLICK FUNCTION
 const selectDay = () => {
   let giorni = document.getElementsByClassName("day");
 
   for (let i = 0; i < giorni.length; i++) {
-    giorni[i].addEventListener("click", function () {
-      let selectedDay = document.querySelector(".selected");
+    giorni[i].addEventListener("click", function (e) {
+      let selectedDay = document.querySelector(".selected"); // variabile che rappresenta il giorno già selezionato (se è stato selezionato)
+
       this.classList.add("selected");
-      lastSelDay = parseInt(this.innerText);
-      lastSelMonthYear = document.querySelector("h1").innerText;
+      lastSelDay = parseInt(this.innerText); //variabile globale per tenere traccia dell'ultimo giorno selezionato
+      lastSelMonthYear = document.querySelector("h1").innerText; //variabile globale per tenere traccia del mese del giorno selezionato
       if (this === selectedDay) {
+        //condizione quando clicchiamo il giorno già selezionato
         this.classList.remove("selected");
         lastSelDay = null;
         lastSelMonthYear = null;
       } else if (selectedDay) {
+        //condizione quando clicchiamo un giorno diverso, ed esiste già un giorno selezionato (diverso)
         selectedDay.classList.remove("selected");
-        this.classList.add("selected");
         lastSelDay = parseInt(this.innerText);
         lastSelMonthYear = document.querySelector("h1").innerText;
       }
 
       changeDayNumber();
+      showAppointments(lastSelDay, monthNames[actualMonth]);
     });
   }
 };
 
+// FUNZIONE PER CAMBIARE IL GIORNO SELEZIONATO SUL FORM SOTTOSTANTE
 const changeDayNumber = () => {
   const meetingDay = document.getElementById("newMeetingDay");
 
@@ -173,37 +190,33 @@ const changeDayNumber = () => {
   }
 };
 
-const nextMonth = document
-  .getElementById("forward")
-  .addEventListener("click", function (e) {
-    if (actualMonth === 11) {
-      actualMonth = 0;
-      actualYear += 1; // Aggiorna year
-    } else {
-      actualMonth += 1; // Aggiorna month
-    }
+const nextMonth = document.getElementById("forward").addEventListener("click", function (e) {
+  if (actualMonth === 11) {
+    actualMonth = 0;
+    actualYear += 1; // Aggiorna year
+  } else {
+    actualMonth += 1; // Aggiorna month
+  }
 
-    return createDaysAndMonth(actualMonth, actualYear);
-  });
+  return createDaysAndMonth(actualMonth, actualYear);
+});
 
-const prevMonth = document
-  .getElementById("back")
-  .addEventListener("click", function (e) {
-    if (actualMonth === 0) {
-      actualMonth = 11;
-      actualYear -= 1; // Aggiorna year
-    } else {
-      actualMonth -= 1; // Aggiorna month
-    }
+const prevMonth = document.getElementById("back").addEventListener("click", function (e) {
+  if (actualMonth === 0) {
+    actualMonth = 11;
+    actualYear -= 1; // Aggiorna year
+  } else {
+    actualMonth -= 1; // Aggiorna month
+  }
 
-    return createDaysAndMonth(actualMonth, actualYear);
-  });
+  return createDaysAndMonth(actualMonth, actualYear);
+});
 
 // 👇👇👇 punto di ingresso del nostro codice
 window.onload = function () {
   createDaysAndMonth(now.getMonth(), now.getFullYear());
 
-  changeDayNumber();
+  // changeDayNumber();
 
   // tutte le risorse sono a questo punto già caricate nel browser
   // è il momento più sicuro per cominciare a cercare gli elementi nel DOM
